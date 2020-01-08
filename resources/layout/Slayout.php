a26c51c2f20df3be7226a3be1449a7f0ffb319d7
pouria <51161936+Pousparker@users.noreply.github.com>
2020-01-09 02:11:50 +0330

    Add files via upload

---
+++ resources/layout/Slayout.php
@@ -0,0 +1,74 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="../lib/css/uikit.css" />
+    <link rel="stylesheet" href="../lib/css/meCss/localCss.css">
+    <link rel="stylesheet" href="../../lib/css/uikit.css" />
+    <link rel="stylesheet" href="../../lib/css/meCss/localCss.css">
+    <!-- UIkit JS -->
+    <script src="../lib/js/uikit.min.js"></script>
+    <script src="../lib/js/uikit-icons.min.js"></script>
+    <script src="../../lib/js/uikit.min.js"></script>
+    <script src="../../lib/js/uikit-icons.min.js"></script>
+
+    <title>Document</title>
+</head>
+<body>
+<nav class="uk-navbar-container" uk-navbar style="background-color:#FFFFFF;">
+    <div class="uk-navbar-left">
+
+        <ul class="uk-navbar-nav">
+            <li class="uk-active"><a href="../home/index">Home</a></li>
+            <li>
+                <a href="#">About</a>
+            </li>
+        </ul>
+
+    </div>
+    <div class="uk-navbar-right">
+
+        <ul class="uk-navbar-nav">
+            <li>
+                <div class="uk-grid-small uk-flex-middle" uk-grid>
+                    <div class="uk-width-auto">
+                        <img class='uk-border-circle' width='40' height='40' src='../img/vector/man.svg'>
+                    </div>
+                    <div class="uk-width-expand">
+                        <h3 class="uk-card-title uk-margin-remove-bottom uk-margin-right">pouira</h3>
+                    </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+</div>
+<div class="uk-text-center" uk-grid>
+    <div class="uk-width-1-6@m">
+
+        <div>
+            <div class="uk-card uk-card-default uk-card-hover uk-card-body  uk-position-center-left uk-margin-medium-left" style="border-radius: 10px;">
+                <ul class="uk-iconnav uk-iconnav-vertical">
+                    <li class="uk-margin-down"><a href="../student/comment" uk-icon="icon: comment"></a></li>
+                    <li class="uk-margin-top"><a href="../student/image" uk-icon="icon: image"></a></li>
+<!--                    <li class="uk-margin-top"><a href="../teacher/get" uk-icon="icon: upload"></a></li>-->
+                    <li class="uk-margin-top"><a href="../student/setting" uk-icon="icon: user"></a></li>
+                </ul>
+
+            </div>
+        </div>
+
+
+    </div>
+    <div class="uk-width-5-6@m">
+
+        <div class=" uk-margin-right uk-margin-top">
+
+            {content}
+        </div>
+    </div>
+
+</div>
+</body>
+</html>
---
+++ resources/layout/singup.php
@@ -0,0 +1,53 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="../lib/css/uikit.css" />
+    <link rel="stylesheet" href="../lib/css/meCss/localCss.css">
+    <!-- UIkit JS -->
+    <script src="../lib/js/uikit.min.js"></script>
+    <script src="../lib/js/uikit-icons.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<div class="uk-container-expand">
+    <!--navbar-->
+    <nav class="uk-container">
+        <div class="uk-navbar-center">
+
+            <div class="uk-navbar-center-left">
+                <div>
+                    <ul class="uk-navbar-nav">
+                        <li class="uk-active">
+                            <a href="index" class="uk-button uk-button-text">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" class="uk-button uk-button-text">Teachers</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <a class=" uk-logo" href="#">
+                <img src="../img/vector/bird.svg" width="45" height="68" alt="">
+            </a>
+            <div class="uk-navbar-center-right">
+                <div>
+                    <ul class="uk-navbar-nav">
+                        <li>
+                            <a href="#" class="uk-button uk-button-text">Commetn</a>
+                        </li>
+                        <li>
+                            <a href="#" class="uk-button uk-button-text">About</a>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
+</body>
+
+</html>
+{content}
