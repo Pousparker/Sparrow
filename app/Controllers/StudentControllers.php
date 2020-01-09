641da7f142ca073e644e7063b31ee6a8a7322667
pouria <51161936+Pousparker@users.noreply.github.com>
2020-01-10 01:28:43 +0330

    Update StudentControllers.php

--- app/Controllers/StudentControllers.php
+++ app/Controllers/StudentControllers.php
@@ -14,8 +14,8 @@ class StudentControllers extends BaseControllers
     public function comment($ID)
     {
         $image=DB::table("comments")->where("ID","=",$ID)->get();
-
-        return $this->view("student/comment",['re'=>$image,'id'=>$ID]);
+        $reply=DB::table("reply")->where("com_id","=",$ID)->get();
+        return $this->view("student/comment",['re'=>$image,'id'=>$ID,'reply'=>$reply]);
 
     }
     public function image ( )
@@ -28,6 +28,19 @@ class StudentControllers extends BaseControllers
         $message=Request::get_param('text');
 
         DB::table('comments')->update("comment",$message,$ID);
+        return $this->redirect("../comment/{$ID}");
+    }
+    public function replay($ID)
+    {
+        session_start();
+
+        $iduser=$_SESSION['ID'];
+        $reply=Request::get_param('reply');
+        $field=['com_id','user_id','replay'];
+        $record=[$ID,$iduser,$reply];
+
+        DB::table('reply')->insert($field,$record);
+
         return $this->redirect("../comment/{$ID}");
     }
 }
